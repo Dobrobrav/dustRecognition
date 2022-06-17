d6093bf166ebbbc2b76d67ec138161e1b2a1f192
user <kalyakulin.semen@gmail.com>
2022-06-17 17:46:23 +0700

    developing...

--- dustRecognition/main 2.py
+++ dustRecognition/main 2.py
@@ -6,6 +6,73 @@ from typing import TypeAlias
 
 
 Picture: TypeAlias = ndarray
+DustSize: TypeAlias = int
+Quantity: TypeAlias = int
+
+
+class Cell:
+    i: int
+    j: int
+    color: int
+    dust_piece: 'DustPiece | None' = None
+
+    def __init__(self, i: int, j: int, color: int):
+        self.i = i
+        self.j = j
+        self.color = color
+
+    @property
+    def dust_piece(self) -> 'DustPiece | None':
+        return self.dust_piece
+
+    @dust_piece.setter
+    def dust_piece(self, dust_piece: 'DustPiece') -> None:
+        self.dust_piece = dust_piece
+        dust_piece.add_cell(self)
+
+
+
+class DustPiece:
+    current_pk = 0
+    cells: list[Cell]
+
+    def __init__(self):
+        self.__class__.current_pk += 1
+        self.pk = self.__class__.current_pk
+        self.cells = []
+
+    def add_cell(self, cell: Cell) -> None:
+        self.cells.append(cell)
+
+
+class DustField:
+    dust_pieces: list[Cell]
+    field: ndarray
+
+    def __init__(self, field: Picture):
+        for i, row in enumerate(self.field):
+            for j, color in enumerate(row):
+                row[j] = Cell(i, j, color)
+        self.field = field
+
+    def distribute_dust_by_size(self) -> dict[DustSize, Quantity]:
+        for row in self.field:
+            for cell in row:
+                if cell.color == 255 or cell.dust_piece:
+                    continue
+
+                self.recognize_dust_piece(cell)
+
+    def get_neighbors(self, cell) -> list[Cell]:
+        """ Return only neighbours which haven't found their dust piece yet"""
+        pass
+
+    def recognize_dust_piece(self, cell: Cell, dust_piece: DustPiece = None) -> None:
+        """ Recognize the entire dust piece which contains the cell """
+        cell.dust_piece = dust_piece or DustPiece()
+        neighbors = self.get_neighbors(cell)
+        for neighbor in neighbors:
+            self.recognize_dust_piece(neighbor, dust_piece=cell.dust_piece)
 
 
 def read_img(file_name: str) -> Picture:
@@ -27,10 +94,8 @@ def get_filtered(img: Picture, bound: int = 180, filter: bool = True) -> Picture
     copy_img = img.copy()
 
     for row in copy_img:
-        # print(row)
         for j, pixel_value in enumerate(row):
             if pixel_value > bound:
-                # print(pixel_value)
                 row[j] = 255
 
     return copy_img
@@ -45,30 +110,33 @@ def show(img: Picture, name: str = "some_img") -> None:
 
 
 def main():
-    BOUND = 155
+    BOUND = 160
+    B = 10
     FILTERED_BLURRED_NAME = f"Afiltered Blurred Bound: {BOUND}"
     FILTERED_NON_BLURRED_NAME = f"Afiltered Non-Blurred Bound: {BOUND}"
 
     raw_img = read_img("img.jpg")
 
     gs_img = get_grayscale(raw_img)
-    blurred_img = get_blurred(gs_img, b=6, blur=False)
+    blurred_img = get_blurred(gs_img, b=B, blur=True)
     filtered_blurred_img = get_filtered(blurred_img, bound=BOUND, filter=True)
-    filtered_non_blurred_img = get_filtered(blurred_img, bound=BOUND, filter=False)
 
     save_to_file(filtered_blurred_img, f"{FILTERED_BLURRED_NAME}.png")
 
     show(gs_img, f"grayscale")
-    # show(blurred_img, f"blurred")
     show(filtered_blurred_img, FILTERED_BLURRED_NAME)
-    show(filtered_non_blurred_img, FILTERED_NON_BLURRED_NAME)
+
+    print(type(gs_img[0]))
+
+
+
 
     # print(filtered_img[0])
 
 
 
 
-    # cv.imshow("canny_orig", cv.Canny(img, 10, 200))
+    cv.imshow("canny_orig", cv.Canny(filtered_blurred_img, 10, 200))
     # cv.imshow("canny_filt", cv.Canny(filtered, 10, 200))
 
     cv.waitKey(0)
